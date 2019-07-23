6eefbf952b69dcbd2eb21d38e2541ff719182066
ilyalychkou <ilya.lychkou@gmail.com>
2019-07-23 20:26:47 +0300

    содержимое helloci-master

---
+++ src/test/java/epam/automatedtesting/training/AppTest.java
@@ -0,0 +1,17 @@
+package epam.automatedtesting.training;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testShouldBePassed() {
+        {
+            assertTrue(true);
+        }
+
+    }
+
+}
